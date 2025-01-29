b813470b5d61b2a7b3ea4124561cc44d4aef87e3
Yaswanth7989 <vikaram7989@gmail.com>
2025-01-29 17:25:59 +0530

    footer about

---
+++ deepu.js
@@ -0,0 +1,72 @@
+// //console.log('Hello word');
+// //alert('Hello code with yash')
+// //document.write('Hello word document');
+
+// let greetings =prompt("enter your name");
+// console.log('Hello' +   greetings);
+ // Add the two numbers
+//     let a=Number(prompt("Enter value A") );
+//     let b=Number(prompt("Enter value B" ));
+//     let c=Number(prompt("Enter value C") );
+//  const sum=a+b+c;
+ 
+//  console.log(sum);
+// squre root 
+// let   a= Number(prompt('Enter a number')) ;
+// const sqrt=Math.sqrt(a);
+// console.log(sqrt);
+// Area of Triangle
+//  let Area=Number;
+//  let base=2;
+//  let Height=8;
+//   Area=(base*Height)/2;
+//   console.log(Area);
+  // Swap  two variable
+//   let a =10;
+//   let b=20;
+//   let c =a
+//   a=b;
+//   b=c;
+  
+//   console.log(a,b);
+  
+//  Truth values
+//var something = 45   !='45';
+// var something =45 ==='45';
+// console.log(something);
+// let money =30;
+// if(money){
+//   console.log("Dont spend  to much");
+//   {
+//     console.log("Dont the job");
+    
+//   }
+  
+// }
+// myName='yash';
+// myHeroAge=32;
+// mysecHero='venky';
+
+
+// console.log(myName +''+ myHeroAge);
+// //Fucncations
+//  function Add(){
+//  return 2+5;
+//  }
+//  console.log("the sum of Number" +Add());
+//  let sum =Add(); 
+//  console.log("the sum of Number" +sum);
+ 
+// function fullName( firstName,lastName){
+//   return firstName +''+lastName;
+// }
+// console.log(fullName('Naveen', 'Kumar'));
+// function washClothes(){
+//   console.log("washing CLothes...",dirtyShirt)
+//   console.log("washing Completed!");
+//   return "Cleanclothes"
+// }
+// let dirtyShirt ="Dirty shirt"
+// let cleanShirt=washClothes(dirtyShirt);
+
+// console.log("washed clothes",cleanShirt);
